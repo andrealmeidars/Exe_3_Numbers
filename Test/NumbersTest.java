ac40b6731fcf6a7ddee1d88a5e7cf44ea261439f
Andre Almeida <andre.almeida@msn.com>
2015-10-17 17:20:14 -0300

    Create test return0With2Numbers

---
+++ Test/NumbersTest.java
@@ -0,0 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class NumbersTest {
+
+    private Numbers numbers;
+
+    @Before
+    public void setUp() throws Exception {
+        this.numbers = new Numbers();
+
+    }
+
+    @Test
+    public void return0With2Numbers(){
+        assertThat(numbers.getSumNumbers(1,2), is(0));
+    }
+
+
+
+
+}
--- src/Numbers.java
+++ src/Numbers.java
@@ -1,3 +1,18 @@
 
 public class Numbers {
+    private int numberA;
+    private  int numberB;
+
+
+
+
+
+    public int getSumNumbers(int numberA, int numberB){
+
+        return 0;
+
+    }
 }
+
+
+
